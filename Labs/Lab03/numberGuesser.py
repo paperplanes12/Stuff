3e4d485974a02a7e4b8e9925af2d53fa9e8a8632
Tye Bastola <z5208424@student.unsw.edu.au>
2018-08-12 15:07:20 +1000

    finished stuff

--- Labs/Lab03/numberGuesser.py
+++ Labs/Lab03/numberGuesser.py
@@ -1,11 +1,54 @@
-def run_game()
+import random
 
-    return
+MIN = 0
+MAX = 10
+NUM_VALUES = 3
 
-def handle_guess(list lista, int)
+def handle_guess(guess, values):
+    # This function should return a duplicate list of values
+    # with the guessed value removed if it was present.
+    if (values.count(guess) > 0):
+        values.remove(guess)
+    return values
 
-    return
+def find_closest(guess, values):
+    # This function should return the closest value
+    # to the guessed value.
+    return min(values, key=lambda x:abs(x-guess))
 
-def find_closest()
+def run_game(values):
+    # While there are values to be guessed and the user hasn't
+    # quit (with 'q'), the game should wait for the user to input
+    # their guess and then reveal whether the closest number is
+    # lower or higher.
+    print(f'Numbers are between {MIN} and {MAX} inclusive. There are {len(values)} values left.')
+    check = 0
+    while (check == 0):
+        guess = input('Guess: ')
+        if (guess == 'q'):
+            return
+        guess = int(guess)
+        #if value is inside the list
+        if (values.count(guess) > 0):
+            print(f'You found {guess}! It was in the list.')
+            handle_guess(guess, values)
+        else:
+            closest = find_closest(guess, values)
+            s = ""
+            if (closest > guess):
+                s = "higher"
+            else:
+                s = "lower"
+            print(f"The closest to {guess} is {s}")
 
-    return
+        if (len(values) > 0):
+            print(f'There are {len(values)} left.')
+        else:
+            print('Congratulations! You won!')
+            check = 1
+        
+    print('Thanks for playing! See you soon.')
+
+if __name__ == '__main__':
+    values = sorted(random.sample(range(MIN, MAX+1), NUM_VALUES))
+    run_game(values)
---
+++ Labs/Lab03/useCase.py
@@ -0,0 +1,27 @@
+Actors: customer, staff, manager, QEBI insurances, database, external credit card system,
+
+Goals
+1) customer specifies search criteria and views available cars
+2) customer select and book car 
+3) customer inputs personal info and requirements (name, age, license number, rental period, option to purchase insurance, email address)
+4) insurance provided by QEBI insurance
+5) compute and display cost
+6) customer input of credit card details
+7) handle payments
+? email sent to customer
+9) staff log on to website and enter new car information
+10) online system for staff to update website and internal inventory
+11) manager generates weekly reports
+
+Use Case Name: Book car
+Brief Description: Customer browse for car that matches their requirement, select car, provides detail and books car
+Initiating Actor: Customer 
+Actor's goal: To rent car
+Participating actors: QEBI insurance
+Flow of events:
+-Customer browses for car with specifications
+-Customer selects car
+-Customer inputs personal information
+-Add insurance (optional)
+-Payment transcation
+-Sends confirmation email
